a7f14047324d569ee7d5b4f8ad927c452818cc26
Safouane Riahi <safouane@atawiz.fr>
2020-09-02 23:16:20 +0400

    fetch products

--- popchef_frontoffice/src/App.js
+++ popchef_frontoffice/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/products")
+      .then(response => response.json())
+      .then(response => setProducts(response))
+
+  }, [])
+  const [products, setProducts] = useState(undefined);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{JSON.stringify(products)}</p>
     </div>
   );
 }
